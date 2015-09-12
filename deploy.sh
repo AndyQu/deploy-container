5c000b9799c2fea98a86a4f8a218e51ff283c703
曲国铖 <quguocheng@meituan.com>
2015-09-12 19:01:44 +0800

    fix path

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,11 @@ TERM=dumb
 #定制产生配置文件
 folder=`dirname $0`
 cd ${folder}
-gradle -Pmain=user_input -PprojectName=${projectName}
+gradle -Pmain=user_input -Pargs=${projectName}
 
 #传递到云主机上
 config_file=${projectName}_`date "+%Y-%m-%d_%H-%M-%S"`.json
-scp /tmp/${projectName}.json root@${host}:/tmp/${config_file}
-
+scp -v /tmp/${projectName}.json root@${host}:/tmp/${config_file}
+echo $?
 #执行部署脚本
-ssh root@${host} "cd /root/docker-deploy;git checkout .;git pull;"
+ssh root@${host} "PATH=/opt/soft/gradle-2.4/bin/:$PATH;echo $PATH;cd /root/docker-deploy;git checkout .;git pull;gradle -Pmain=deploy -Pargs=/tmp/${config_file}"
