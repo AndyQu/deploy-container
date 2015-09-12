db3f0373bb250c9d87a5a028383b84a9cc4b5b10
曲国铖 <quguocheng@meituan.com>
2015-09-12 20:38:16 +0800

    update code

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,6 @@ gradle -Pmain=user_input -Pargs=${projectName}
 
 #传递到云主机上
 config_file=${projectName}_`date "+%Y-%m-%d_%H-%M-%S"`.json
-scp -v /tmp/${projectName}.json root@${host}:/tmp/${config_file}
-echo $?
+scp /tmp/${projectName}.json root@${host}:/tmp/${config_file}
 #执行部署脚本
-ssh root@${host} "export JAVA_HOME=/usr/java/jdk1.7.0_79;PATH=/opt/soft/gradle-2.4/bin/:$PATH;echo $PATH;cd /root/docker-deploy;gradle -Pmain=deploy -Pargs=/tmp/${config_file}"
+ssh root@${host} "export JAVA_HOME=/usr/java/jdk1.7.0_79;PATH=/opt/soft/gradle-2.4/bin/:$PATH;echo $PATH;cd /root/docker-deploy;git checkout .;git pull;gradle -Pmain=deploy -Pargs=/tmp/${config_file}"
