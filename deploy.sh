17720318c65d46be0d8751d1743308dbe678f55f
曲国铖 <quguocheng@meituan.com>
2015-09-12 22:05:01 +0800

    change log

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,10 @@ TERM=dumb
 #定制产生配置文件
 folder=`dirname $0`
 cd ${folder}
-gradle -Pmain=user_input -Pargs=${projectName}
+gradle -Pmain=user_input -Pargs=${projectName} run
 
 #传递到云主机上
 config_file=${projectName}_`date "+%Y-%m-%d_%H-%M-%S"`.json
 scp /tmp/${projectName}.json root@${host}:/tmp/${config_file}
 #执行部署脚本
-ssh root@${host} "export JAVA_HOME=/usr/java/jdk1.7.0_79;PATH=/opt/soft/gradle-2.4/bin/:$PATH;echo $PATH;cd /root/docker-deploy;git checkout .;git pull;gradle -Pmain=deploy -Pargs=/tmp/${config_file}"
+ssh root@${host} "export JAVA_HOME=/usr/java/jdk1.7.0_79;PATH=/opt/soft/gradle-2.4/bin/:$PATH;echo $PATH;cd /root/docker-deploy;git checkout .;git pull;gradle -Pmain=deploy -Pargs=/tmp/${config_file} run"
--- src/main/groovy/com/sankuai/srq/deploy/script/deploy.groovy
+++ src/main/groovy/com/sankuai/srq/deploy/script/deploy.groovy
@@ -3,10 +3,16 @@ import com.sankuai.srq.deploy.DeployEngine
 import com.sankuai.srq.deploy.ProjectMeta
 import groovy.json.JsonSlurper
 
+import java.lang.management.ManagementFactory
+import java.lang.management.RuntimeMXBean
+
 if (args.size() <= 0) {
     println("请指定要部署的工程配置文件")
     System.exit(1)
 }
+RuntimeMXBean rt = ManagementFactory.getRuntimeMXBean();
+String pid = rt.getName();
+MDC.put("PID", pid);
 def jsonSlurper = new JsonSlurper();
 def jsonData = jsonSlurper.parse(new FileReader(new File(args[0])))
 
