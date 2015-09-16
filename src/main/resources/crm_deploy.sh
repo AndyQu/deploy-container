f5ca81dbbd78cc5d01c1924216c3b5b0a764989c
曲国铖 <quguocheng@meituan.com>
2015-09-16 17:49:05 +0800

    name

--- src/main/resources/crm_deploy.sh
+++ src/main/resources/crm_deploy.sh
@@ -1,7 +1,7 @@
 #@IgnoreInspection BashAddShebang
 #必须在srqcrm工程根目录下面执行
 ./gradlew clean -x test -x check build
-cp build/libs/srqcrm-1.0.war $JETTY_HOME/webapps/sup.war
+cp build/libs/srcrmserver-1.0.war $JETTY_HOME/webapps/sup.war
 
 cd $JETTY_HOME
 ./bin/jetty.sh start
