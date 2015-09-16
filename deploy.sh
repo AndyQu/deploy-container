37f821630e6667ed42ee6e5c2a53defbc7e2434c
曲国铖 <quguocheng@meituan.com>
2015-09-16 19:30:03 +0800

    加help

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
-if [ $# -lt 1 ]; then
-    echo "请指定要部署的工程名称. 目前可部署的工程包括:srqserver,h5,crm,jxc,srcms,srqserver_h5"
+projects=(srqserver h5 crm jxc srcms srqserver_h5)
+function help {
+    echo "可以部署的项目如下:"
+    for project in ${projects[@]}; do
+        echo "    "${project}
+    done
+    echo "例如 ./deploy crm"
+    echo ""
+}
+if [ $# -lt 1 ] || [ $1 == "list" ]; then
+    help
     exit 1
 fi
+
 projectName=$1
+projectExist=0
+for project in ${projects[@]}; do
+    if [ ${project} == ${projectName} ];then
+        projectExist=1
+        break
+    fi
+done
+
+if [ ${projectExist} -ne 1 ];then
+    echo "~~~亲,这个项目不存在哦:${projectName}"
+    echo ""
+    help
+    exit 1
+fi
+
 host=172.27.2.94
 
 echo "开始部署工程:${projectName}"
@@ -19,3 +44,5 @@ config_file=${projectName}_`date "+%Y-%m-%d_%H-%M-%S"`.json
 scp /tmp/${projectName}.json root@${host}:/tmp/${config_file}
 #执行部署脚本
 ssh root@${host} "export JAVA_HOME=/usr/java/jdk1.7.0_79;PATH=/opt/soft/gradle-2.4/bin/:$PATH;echo $PATH;cd /root/docker-deploy;git checkout .;git pull;gradle -Pmain=deploy -Pargs=/tmp/${config_file} run"
+
+
