6a2fff8e9ea6c159cdd1bf27d802ebb5fe4b2cbe
quguocheng <quguocheng@meituan.com>
2015-11-02 11:23:52 +0800

    use gradlew

--- deploy.sh
+++ deploy.sh
@@ -37,12 +37,12 @@ TERM=dumb
 #定制产生配置文件
 folder=`dirname $0`
 cd ${folder}
-gradle -Pmain=user_input -Pargs=${projectName} run
+./gradlew -Pmain=user_input -Pargs=${projectName} run
 
 #传递到云主机上
 config_file=${projectName}_`date "+%Y-%m-%d_%H-%M-%S"`.json
 scp /tmp/${projectName}.json root@${host}:/tmp/${config_file}
 #执行部署脚本
-ssh root@${host} "export JAVA_HOME=/usr/java/jdk1.7.0_79;PATH=/opt/soft/gradle-2.4/bin/:$PATH;echo $PATH;cd /root/docker-deploy;git checkout .;git pull;gradle -Pmain=deploy -Pargs=/tmp/${config_file} run"
+ssh root@${host} "export JAVA_HOME=/usr/java/jdk1.7.0_79;PATH=/opt/soft/gradle-2.4/bin/:$PATH;echo $PATH;cd /root/docker-deploy;git checkout .;git pull;./gradlew -Pmain=deploy -Pargs=/tmp/${config_file} run"
 
 
