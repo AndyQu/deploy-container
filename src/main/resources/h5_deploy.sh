bbd3d1511a4ec480ff3f24f5ebfc371c8602dc75
quguocheng <quguocheng@meituan.com>
2015-11-25 11:13:15 +0800

    modify

--- src/main/resources/h5_deploy.sh
+++ src/main/resources/h5_deploy.sh
@@ -5,7 +5,17 @@ nvm use iojs-v2.3.0
 echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
 npm config set registry http://r.npm.sankuai.com
 
-npm install
+
+bs_hash=`git rev-parse --short HEAD`
+
+npm install --production
+
+[[ -f "public/hash.json" ]] && sed -i "s/HEAD\!/$bs_hash/g" /public/hash.json
+
+cat public/hash.json
+
+curl -s --fail -X POST -d "ring=1&target=fe/fe-paidui" -d "hash=$bs_hash" build.sankuai.com/bs/entry/update/all
+
 #mv config/dev.js.sample config/dev.js
 sed 's/sr.test.meituan.com/localhost:8088/g' -i config/test.js
 mkdir -p /usr/local/tmp/
