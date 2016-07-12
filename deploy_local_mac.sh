58ef66f6d067b1e81344289d35c5610e7f76b37b
quguocheng <quguocheng@meituan.com>
2016-07-12 10:10:52 +0800

    make config_file full path

--- deploy_local_mac.sh
+++ deploy_local_mac.sh
@@ -40,13 +40,16 @@ cp src/main/resources/localhost_mac/envConf.json src/main/resources/envConf.json
 ./gradlew --console=plain -Pmain=user_input -Pargs="${projectName},src/main/resources/envConf.json" run
 
 #传递到主机上
-config_file=${projectName}_`date "+%Y-%m-%d_%H-%M-%S"`.json
-cp /tmp/${projectName}.json /tmp/${config_file}
+mkdir -p /tmp/docker-deploy/config/
+config_file=/tmp/docker-deploy/config/${projectName}_`date "+%Y-%m-%d_%H-%M-%S"`.json
+
+
+cp /tmp/${projectName}.json ${config_file}
 
 #执行部署脚本
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home/
 PATH=/usr/local/bin/:$PATH
 echo $PATH
-./gradlew -Pmain=deploy -Pargs=/tmp/${config_file} run
+./gradlew -Pmain=deploy -Pargs=${config_file} run
 
 
