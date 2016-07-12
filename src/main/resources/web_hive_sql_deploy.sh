d62850d15dfd0a26dcf5a8f6d642dc516a7e5e99
quguocheng <quguocheng@meituan.com>
2016-07-12 10:10:08 +0800

    use screen

--- src/main/resources/web_hive_sql_deploy.sh
+++ src/main/resources/web_hive_sql_deploy.sh
@@ -1,4 +1,4 @@
 #@IgnoreInspection BashAddShebang
 #必须在srqserver工程根目录下面执行
 ./gradlew clean build
-nohup java -server -jar build/libs/hivesql-0.1.0.jar &1
+screen -dmS web java -server -jar build/libs/hivesql-0.1.0.jar
