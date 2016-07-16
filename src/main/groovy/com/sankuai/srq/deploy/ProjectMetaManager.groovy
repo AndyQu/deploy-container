86adbbf9e4fe8fb2e894e36cd10475e07ce1d285
quguocheng <quguocheng@meituan.com>
2016-07-16 15:20:21 +0800

    log4j->logback

--- src/main/groovy/com/sankuai/srq/deploy/ProjectMetaManager.groovy
+++ src/main/groovy/com/sankuai/srq/deploy/ProjectMetaManager.groovy
@@ -2,7 +2,10 @@ package com.sankuai.srq.deploy
 //import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 
+import org.slf4j.LoggerFactory
+
 class ProjectMetaManager {
+	def static final logger = LoggerFactory.getLogger("DeployEngine")
 	
 	def static ProjectMetaManager _ins=null
 	
@@ -19,16 +22,15 @@ class ProjectMetaManager {
 	}
 	
 	def updateData(){
-		println "mkdir -p ${pFolder}".execute().text
+		new File(pFolder).mkdirs()
 		
 		def f = new File(metasFolder)
 		if(! f.exists()){
-			println "${f} 不存在"
-			println "git clone https://github.com/AndyQu/deploy_sys_project_meta.git".execute(null, new File(pFolder)).text
+			logger.info "${f} 不存在"
+			logger.info "git clone https://github.com/AndyQu/deploy_sys_project_meta.git".execute(null, new File(pFolder)).text
 		}else{
-			//TODO
-			println "git checkout master".execute(null, new File(metasFolder)).text
-			println "git pull".execute(null, new File(metasFolder)).text
+			logger.info "git checkout master".execute(null, new File(metasFolder)).text
+			logger.info "git pull".execute(null, new File(metasFolder)).text
 		}
 		_parse()
 	}
@@ -38,28 +40,28 @@ class ProjectMetaManager {
 		new File("${metasFolder}/metas").eachDir {
 			File projectFolder->
 				if(!new File("${projectFolder.absolutePath}/meta.json").exists() ){
-					println "文件 ${projectFolder.absolutePath}/meta.json 不存在"
+					logger.warn "文件 ${projectFolder.absolutePath}/meta.json 不存在"
 				}else if(!new File("${projectFolder.absolutePath}/deploy.sh").exists() ){
-					println "文件 ${projectFolder.absolutePath}/deploy.sh 不存在"
+					logger.warn "文件 ${projectFolder.absolutePath}/deploy.sh 不存在"
 				}else{
 					def metaJson = jsonSlurper.parse(new FileReader("${projectFolder.absolutePath}/meta.json"))
 					nameMetaMap[metaJson.projectName.toLowerCase()]=metaJson
 					nameBashMap[metaJson.projectName.toLowerCase()]="${projectFolder.absolutePath}/deploy.sh"
-					println "读取到Project信息：${metaJson.projectName}"
+					logger.info "读取到Project信息：${metaJson.projectName}"
 				}
 		}
 	}
 	
 	def Collection<String> getAllProjectNames(){
-		println "获取所有项目名称:${nameBashMap.keySet()}"
+		logger.info "获取所有项目名称:${nameBashMap.keySet()}"
 		nameBashMap.keySet()
 	}
 	def Collection<ProjectMeta> getProjectMetas(Collection<String>projectNames){
-		println "获取项目Meta：${projectNames}"
+		logger.info "获取项目Meta：${projectNames}"
 		nameMetaMap.subMap(projectNames.collect {it->it.toLowerCase()}).values()
 	}
 	def String getProjectBashFile(String projectName){
-		println "获取项目Bash脚本：${projectName}"
+		logger.info "获取项目Bash脚本：${projectName}"
 		nameBashMap[projectName.toLowerCase()]
 	}
 }
