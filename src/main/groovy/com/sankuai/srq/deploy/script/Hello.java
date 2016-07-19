57853069b98453b10c23029d67e4d509c2316a82
quguocheng <quguocheng@meituan.com>
2016-07-19 09:13:50 +0800

    aa

--- src/main/groovy/com/sankuai/srq/deploy/script/Hello.java
+++ src/main/groovy/com/sankuai/srq/deploy/script/Hello.java
@@ -1,12 +1,7 @@
 package com.sankuai.srq.deploy.script;
 
 public class Hello {
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		System.out.println("Hello,java");
-=======
-	public static void main(String[]args){
-		System.out.println("hello");
->>>>>>> c20face75362baa8a099705d190ef6287fe06caa
 	}
 }
