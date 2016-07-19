7033e1c4c7f3c32886a7c8d6cccee97bb373f9c4
quguocheng <quguocheng@meituan.com>
2016-07-19 10:46:35 +0800

    去除去Host主机Groovy的依赖

--- deploy_local_ubuntu.sh
+++ deploy_local_ubuntu.sh
@@ -6,7 +6,8 @@ envConfFileName="src/main/resources/localhost_ubuntu/envConf.json"
 targetProjectNames=$1
 ./gradlew writeClasspath
 ./gradlew clean jar
-echo `cat /tmp/classpath.txt`
-java -cp `cat /tmp/classpath.txt`:/usr/local/Cellar/groovy/2.4.3/libexec/lib/*:./build/libs/deploy_system-1.0-SNAPSHOT.jar com.sankuai.srq.deploy.script.TopControl ${projectRootFolder} ${envConfFileName} ${targetProjectNames}
+
+java -cp `cat /tmp/classpath.txt`:./build/libs/deploy_system-1.0-SNAPSHOT.jar com.sankuai.srq.deploy.script.TopControl ${projectRootFolder} ${envConfFileName} ${targetProjectNames}
+
 #./gradlew --console=plain -Pmain=top_control -Pargs="${projectRootFolder},${envConfFileName},${targetProjectNames}" run
 #在美团办公云ubuntu机器上使用./gradlew构建，Host上的gradlew、Container中的gradlew会产生锁冲突
