7f5c999c9452217df4240abcc6f92e617d0833ae
andyqu <aichongqu@gmail.com>
2016-07-24 12:49:32 +0800

    remove projectRootFolder para

--- deploy_local_mac.sh
+++ deploy_local_mac.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-projectRootFolder=`pwd`
 envConfFileName="src/main/resources/localhost_mac/envConf.json"
 targetProjectNames=$1
-./gradlew --console=plain -Pmain=top_control -Pargs="${projectRootFolder},${envConfFileName},${targetProjectNames}" run
+./gradlew --console=plain -Pmain=top_control -Pargs="${envConfFileName},${targetProjectNames}" run
