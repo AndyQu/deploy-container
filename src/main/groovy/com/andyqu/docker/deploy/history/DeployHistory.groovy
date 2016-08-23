a216184e16fa6a43dc490f5cd3be4abe85d349dd
quguocheng <quguocheng@meituan.com>
2016-08-23 15:48:06 +0800

    fix time bug: int value*1000 will overflow

--- src/main/groovy/com/andyqu/docker/deploy/history/DeployHistory.groovy
+++ src/main/groovy/com/andyqu/docker/deploy/history/DeployHistory.groovy
@@ -23,32 +23,17 @@ class DeployHistory {
 	 * 
 	 * @param startTimeStamp 单位：秒
 	 */
-	public void setStartTimeStamp(int startTimeStamp) {
+	public void setStartTimeStamp(long startTimeStamp) {
 		this.startTimeStamp = startTimeStamp;
 		this.startTime=new Date(startTimeStamp*1000).getDateTimeString()
-	}
-	/**
-	 *
-	 * @param startTimeStamp 单位：秒
-	 */
-	public void setStartTimeStamp(long startTimeStamp) {
-		setStartTimeStamp(startTimeStamp.intValue())
 	}
 
 	/**
 	 *
 	 * @param startTimeStamp 单位：秒
 	 */
-	public void setEndTimeStamp(int endTimeStamp) {
+	public void setEndTimeStamp(long endTimeStamp) {
 		this.endTimeStamp = endTimeStamp;
 		this.endTime=new Date(endTimeStamp*1000).getDateTimeString()
-	}
-	
-	/**
-	 *
-	 * @param startTimeStamp 单位：秒
-	 */
-	public void setEndTimeStamp(long endTimeStamp) {
-		setEndTimeStamp(endTimeStamp.intValue())
 	}
 }
