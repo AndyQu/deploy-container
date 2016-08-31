1330fd3ca782739ee8032908dc6df976023ccc43
quguocheng <quguocheng@meituan.com>
2016-08-31 17:12:08 +0800

    引入event bus

---
+++ src/main/groovy/com/andyqu/docker/deploy/event/DeployStage.java
@@ -0,0 +1,53 @@
+package com.andyqu.docker.deploy.event;
+
+import groovy.transform.ToString;
+
+@ToString
+public enum DeployStage {
+	Start(1, null),
+	CheckSameNameContainer(2,null),
+	StopAndRemoveContainer(3,null),
+	ApplyPortsFromHost(4,null),
+	CalcMountPoints(5,null),
+	BuildFolders(6,null),
+	SetupContainerConfig(7,null),
+	CreateContainer(8,null),
+	StartContainer(9,null),
+		SaveFailedHistory(10,null),
+	DeployProjectInContainer(10,null),
+	CreateDeployFile(11,null),
+	ExecDeployBashFile(12,null),
+	SaveSuccessHistory(13,null),
+	
+	END(0,null)
+	;
+	private int number;
+	private String description;
+
+	private DeployStage(int n, String desc) {
+		number = n;
+		if(desc==null){
+			description=this.name();
+		}
+		else{
+			description = desc;
+		}
+		
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
